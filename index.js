418a886578bd1055ff59a2b5d95b0e2881fa6cae
Amit bhandari <a0423355@gmail.com>
2023-04-02 11:12:16 +0530

    cors

--- index.js
+++ index.js
@@ -8,9 +8,14 @@ const { CartRouter } = require("./Routes/cart_product.route");
 
 require("dotenv").config()
 
-const cors = require("cors")
+const cors = require('cors');
+const corsOptions ={
+    origin:'http://localhost:3000', 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200
+}
+app.use(cors(corsOptions))
 const app = express()
-app.use(cors())
 
 app.use(express.json())
 app.use("/", getRouter)
