67a664061636f59cb701d0a5ceadd7439aa27558
Amit bhandari <a0423355@gmail.com>
2023-04-02 11:17:59 +0530

    cors

--- index.js
+++ index.js
@@ -14,8 +14,8 @@ const corsOptions ={
     credentials:true,            //access-control-allow-credentials:true
     optionSuccessStatus:200
 }
-app.use(cors(corsOptions))
 const app = express()
+app.use(cors(corsOptions))
 
 app.use(express.json())
 app.use("/", getRouter)
